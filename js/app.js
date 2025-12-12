862ed57d1ad2f0215a7324fee7b670c7bafabcce
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2025-12-12 17:24:52 -0500

    number building, removed eval(), decimal support and clear logic updates

--- js/app.js
+++ js/app.js
@@ -12,49 +12,64 @@ let secondValue = '';
 
 let operator = '';
 
-let result = eval(firstValue + operator + secondValue)
+calculator.addEventListener('click', (event) =>{
 
-calculator.addEventListener('click', (event) => {
-    
     //console.log(event.target);
 
-    if (event.target.classList.contains('number')) {
+    if(event.target.classList.contains('number')){
         if(firstValue === '' || operator === ''){
-            firstValue = event.target.textContent;
-            display.innerText += firstValue;   
-        }else{
-            secondValue = event.target.textContent;
-            display.innerText += secondValue;
+            firstValue += event.target.textContent;
+            display.innerText = firstValue;
+        }if(firstValue !== '' && operator !== ''){
+            secondValue += event.target.textContent;
+            display.innerText = secondValue;
+        }if(event.target.textContent === '.' && !firstValue.includes('.')){
+            firstValue += '.'; 
+            display.innerText = firstValue;
+        }if(event.target.textContent === '.' && secondValue !== '' && !secondValue.includes('.')){
+            secondValue += '.';
+            display.innerText = secondValue
         }
         //console.log(firstValue);
         console.log(secondValue)
     }
-    
+
     if (event.target.classList.contains('operator')) {
-        if(firstValue !== '' && operator === ''){
+        if (firstValue !== '' && operator === '') {
             operator = event.target.textContent;
             display.innerText += operator;
         }
-        if(event.target.textContent === 'C'){
-            firstValue = '';
-            secondValue = '';
-            operator = '';
-            display.innerText = '';
-        }
-        console.log(operator)
     }
 
     if (event.target.classList.contains('equals')) {
-        if(operator === ''){
+        if (operator === '') {
             alert('must select an operator')
         }
-        if(operator !== '' && secondValue === ''){
+        if (operator !== '' && secondValue === '') {
             alert('must input second value')
         }
-        if(operator === '/' && secondValue === '0'){
+        if (operator === '/' && secondValue === '0') {
             alert('cannot divide by zero')
             return
         }
-        display.innerText = eval(firstValue + operator + secondValue)
+        let result;
+        if (operator === '+')
+            result = Number(firstValue) + Number(secondValue);
+        if (operator === '-')
+            result = Number(firstValue) - Number(secondValue);
+        if (operator === '*')
+            result = Number(firstValue) * Number(secondValue);
+        if (operator === '/')
+            result = Number(firstValue) / Number(secondValue);
+        display.innerText = result;
     }
+
+    if (event.target.textContent === 'C') {
+        firstValue = '';
+        secondValue = '';
+        operator = '';
+        display.innerText = '';
+        return;
+    }
+    console.log(operator)
 });
